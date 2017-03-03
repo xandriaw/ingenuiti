c69be72b38568d008b36dbda9d312d30d5c5f509
xandriaw <xandria.webb@gmail.com>
2017-03-03 15:27:05 -0500

    removed

--- R/ingenuiti.R
+++
@@ -1,6 +0,0 @@
-
-library('RODBC')
-library('dplyr')
-
-buko<- odbcConnect(dsn='buko', uid='buko-user', pwd='buko-access')
-inspector<-odbcConnect(dsn='inspector', uid='buko-user', pwd='buko-access')
